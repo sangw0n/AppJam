8bf24c41d5ee99ed19dcf85cd0e66d4c1d4b01a2
ksan09 <kansan060223@gmail.com>
2024-06-23 05:07:46 +0900

    Update ThronUnitEvent

--- APPJam26/Assets/01.Scripts/Unit/Event/UnitSpikeBulletEvent.cs
+++ APPJam26/Assets/01.Scripts/Unit/Event/UnitSpikeBulletEvent.cs
@@ -7,6 +7,7 @@ public class UnitSpikeBulletEvent : UnitEvent
 {
     [SerializeField]
     private GameObject _spikeBulletPrefab;
+    List<GameObject> _spikeObjList = new List<GameObject>();
     private bool _isEnd = false;
 
     public override void Init()
@@ -19,7 +20,7 @@ public class UnitSpikeBulletEvent : UnitEvent
         StartCoroutine(BulletCo());
     }
 
-    WaitForSeconds wait021 = new WaitForSeconds(0.31f);
+    WaitForSeconds wait021 = new WaitForSeconds(0.41f);
     IEnumerator BulletCo()
     {
 
@@ -28,18 +29,20 @@ public class UnitSpikeBulletEvent : UnitEvent
 
         for(int i = 0; i < 5; ++i)
         {
-            // -90도 기준으로 90도까지 5개의 탄환 { -90, -45, 0, 45, 90 }
-            float angle = (-90f + (i * 45f)) * Mathf.Deg2Rad;
+            // -180도 기준으로 0도까지 5개의 탄환 { -90, -45, 0, 45, 90 }
+            float angle = (180f - (i * 45f)) * Mathf.Deg2Rad;
             Vector3 dir = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0f);
 
             GameObject spikeBullet = Instantiate(_spikeBulletPrefab, myUnitPos, Quaternion.identity);
             spikeBullet.transform.right = dir;
 
-            spikeBullet.transform.DOMove(myUnitPos + dir * dist, 0.3f).SetEase(Ease.OutCubic);
-
+            spikeBullet.transform.DOMove(myUnitPos + dir * dist, 0.4f).SetEase(Ease.OutCubic);
+            _spikeObjList.Add(spikeBullet);
         }
 
         yield return wait021;
+        _spikeObjList.ForEach(e => Destroy(e));
+        _spikeObjList.Clear();
         _myUnit.OpponentUnit.HitDamage(_myUnit.UnitData.Strength);
 
         yield return wait021;
