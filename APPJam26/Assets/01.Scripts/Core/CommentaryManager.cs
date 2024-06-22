5ccef7fb88f8c2b6d1d42baca26eb0b0c2bc56cb
sangw0n <pilwon070127@naver.com>
2024-06-22 23:15:18 +0900

    Feat Text Animator

--- APPJam26/Assets/01.Scripts/Core/CommentaryManager.cs
+++ APPJam26/Assets/01.Scripts/Core/CommentaryManager.cs
@@ -3,31 +3,19 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class CommentaryManager : MonoBehaviour
+public class CommentaryManager : MonoSingleton<CommentaryManager>
 {
-    public static CommentaryManager instance { get; private set; }
 
     [SerializeField]
     private TextMeshProUGUI textCommentary;
 
-    public void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Q))
-        {
-            TriggerCommentary("Attack!!!!!!!!!!!! Player01");
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            TriggerCommentary("Attack!!!!!!!!!!!! Player02");
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            TriggerCommentary("ohOHHOHOOHO!!!!!!!!!!!! Player01");
-        }
-    }
-
     public void TriggerCommentary(string commentary)
     {
         textCommentary.text = commentary;
+    }
+
+    protected override void Init()
+    {
+        
     }
 }
