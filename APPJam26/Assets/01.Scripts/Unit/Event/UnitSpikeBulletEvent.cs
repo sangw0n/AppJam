0a24af8341900b6d398ea881ab73b93c67d1efbd
ksan09 <kansan060223@gmail.com>
2024-06-23 05:02:29 +0900

    Update UnitEvent

--- APPJam26/Assets/01.Scripts/Unit/Event/UnitSpikeBulletEvent.cs
+++ APPJam26/Assets/01.Scripts/Unit/Event/UnitSpikeBulletEvent.cs
@@ -1,18 +1,54 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitSpikeBulletEvent : MonoBehaviour
+public class UnitSpikeBulletEvent : UnitEvent
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject _spikeBulletPrefab;
+    private bool _isEnd = false;
+
+    public override void Init()
     {
-        
+        _isEnd = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void InvokeEvent()
     {
-        
+        StartCoroutine(BulletCo());
+    }
+
+    WaitForSeconds wait021 = new WaitForSeconds(0.31f);
+    IEnumerator BulletCo()
+    {
+
+        Vector3 myUnitPos = _myUnit.transform.position;
+        float dist = Vector3.Distance(myUnitPos, _myUnit.OpponentUnit.transform.position);
+
+        for(int i = 0; i < 5; ++i)
+        {
+            // -90도 기준으로 90도까지 5개의 탄환 { -90, -45, 0, 45, 90 }
+            float angle = (-90f + (i * 45f)) * Mathf.Deg2Rad;
+            Vector3 dir = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0f);
+
+            GameObject spikeBullet = Instantiate(_spikeBulletPrefab, myUnitPos, Quaternion.identity);
+            spikeBullet.transform.right = dir;
+
+            spikeBullet.transform.DOMove(myUnitPos + dir * dist, 0.3f).SetEase(Ease.OutCubic);
+
+        }
+
+        yield return wait021;
+        _myUnit.OpponentUnit.HitDamage(_myUnit.UnitData.Strength);
+
+        yield return wait021;
+        _isEnd = true;
+
+    }
+
+    public override bool IsEnd()
+    {
+        return _isEnd;
     }
 }
