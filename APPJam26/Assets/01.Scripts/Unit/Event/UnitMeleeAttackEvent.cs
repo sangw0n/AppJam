1ee47e1a3c20927b2c43ec00612a8c65979a53e4
ksan09 <kansan060223@gmail.com>
2024-06-22 23:09:32 +0900

    Update Melee Attack

--- APPJam26/Assets/01.Scripts/Unit/Event/UnitMeleeAttackEvent.cs
+++ APPJam26/Assets/01.Scripts/Unit/Event/UnitMeleeAttackEvent.cs
@@ -9,10 +9,13 @@ public class UnitMeleeAttackEvent : UnitEvent
     private Vector3 offsetDistanceFromEnemy = new Vector3(1.0f, 0.0f, 0.0f);
 
     private bool    isEnd                   = false;
+    private Transform _unitTransform;
 
     public override void Init()
     {
         isEnd = false;
+        _unitTransform = _myUnit.transform;
+
     }
 
     public override void InvokeEvent()
@@ -22,24 +25,27 @@ public class UnitMeleeAttackEvent : UnitEvent
 
     private WaitForSeconds waitForSeconds00 = new WaitForSeconds(1.6f);
     private WaitForSeconds waitForSeconds01 = new WaitForSeconds(2.0f);
+    private WaitForSeconds waitForSeconds02 = new WaitForSeconds(0.1f);
 
     public IEnumerator Co_Attack()
     {
-        Vector3 _tempPosition               = transform.position;
+        Vector3 _tempPosition               = _unitTransform.position;
         Vector3 _offsetDistanceFromEnemy    = offsetDistanceFromEnemy;
 
         // 적 유닛이 오른쪽에 있으면 
-        if (transform.position.x < _myUnit.OpponentUnit.transform.position.x)
+        if (_unitTransform.position.x < _myUnit.OpponentUnit.transform.position.x)
             _offsetDistanceFromEnemy *= -1;
 
-        transform.DOMove(_myUnit.OpponentUnit.transform.position + _offsetDistanceFromEnemy, 1.5f);
+        _unitTransform.DOMove(_myUnit.OpponentUnit.transform.position + _offsetDistanceFromEnemy, 1.5f);
 
         // 애니메이션 
+        
+        yield return waitForSeconds01;
+        _myUnit.OpponentUnit.HitDamage(_myUnit.UnitData.Strength);
+        yield return waitForSeconds02;
         // 파티클 생성 
 
-        yield return waitForSeconds01;
-
-        transform.DOMove(_tempPosition, 1.5f);
+        _unitTransform.DOMove(_tempPosition, 1.5f);
 
         yield return waitForSeconds00;
 
